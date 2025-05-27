dc761e5338f5a61141eb30f4b21d21020ab4ece8
itellaetxe <inigotellaetxe@gmail.com>
2025-05-27 10:59:02 +0200

    INIT: Initialized repo

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Iñigo Tellaetxe-Elorriaga';
+export const SITE_DESCRIPTION = 'Personal website of Iñigo Tellaetxe-Elorriaga';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
